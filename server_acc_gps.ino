14b36b19e290c34fef0d7809ff178e2fba866e5a
HailNicole <mconstantem1@est.ups.edu.ec>
2024-07-28 22:46:28 -0500

    a

--- server_acc_gps.ino
+++ server_acc_gps.ino
@@ -9,8 +9,8 @@
 #include <HardwareSerial.h>
 #include <WiFi.h>   // ESP32
 
-#define WIFI_SSID "ALEXANDER_CNT.FIBRA"
-#define WIFI_PASSWORD "nicolemadeline"
+#define WIFI_SSID "fran"
+#define WIFI_PASSWORD "12345678"
 #define RXD2 16
 #define TXD2 17
 #define NMEA 0
@@ -23,9 +23,9 @@ HardwareSerial neogps(1);
 WiFiClient client;
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
-const char* server = "192.168.1.8"; //Ip de la máquina que está corriendo el servidor
+const char* server = "192.168.104.171"; // IP de la máquina que está corriendo el servidor
 const int port = 8000;
-const int sendingInternval = 2 * 1000;
+const int sendingInterval = 2 * 1000;
 
 void setup() {
   Serial.begin(115200);
@@ -34,11 +34,10 @@ void setup() {
 
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
+    Serial.println(".");
     delay(100);
   }
   Serial.println("\r\nWiFi connected");
-
 
   // Inicialización del sensor MPU6050
   if (!mpu.begin()) {
@@ -47,25 +46,20 @@ void setup() {
   }
   mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
 
-  //Inicialización del GPS NEO-6M
+  // Inicialización del GPS NEO-6M
   neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
 }
 
 void loop() {
-
   // Leer datos del MPU6050
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
 
-  float ax=abs(a.acceleration.x);
-  float ay=abs(a.acceleration.y);
-  float az=abs(a.acceleration.z);
+  float ax = abs(a.acceleration.x);
+  float ay = abs(a.acceleration.y);
+  float az = abs(a.acceleration.z);
 
-  /*x = random(0,50); // ALEATORIOS
-  y = random(0,100); // ALEATORIOS
-  z = random(0,100); // ALEATORIOS*/
-
-    // Detectar cambios bruscos en la aceleración
+  // Detectar cambios bruscos en la aceleración
   if (abs(a.acceleration.x) > 3 || abs(a.acceleration.y) > 3 || abs(a.acceleration.z) > 20) {
     Serial.println(a.acceleration.x);
     Serial.println(a.acceleration.y);
@@ -80,38 +74,45 @@ void loop() {
     // Obtener datos GPS
     while (neogps.available() > 0) {
       gps.encode(neogps.read());
-      Serial.println("Recibiendo datos por serial");
     }
 
     if (gps.location.isUpdated()) {
       Visualizacion_GPS();
+      enviarDatosServer(ax, ay, az, gps.location.lat(), gps.location.lng());
     }
-
-    enviarDatosServer(ax, ay, az);
   }
 }
 
-void enviarDatosServer(float ax, float ay, float az){
+void enviarDatosServer(float ax, float ay, float az, double lat, double lng) {
   if (client.connect(server, port)) {
-    // Crear la cadena de solicitud HTTP
-    String req_uri = "/update?x=" + String(ax, 2) + "&y=" + String(ay, 2) + "&z=" + String(az, 2);
-    client.print("GET " + req_uri + " HTTP/1.1\r\n" + "Host: " + server + "\r\n" + "Connection: close\r\n\r\n");
-    Serial.printf("Datos enviados - X: %s, Y: %s, Z: %s\n", String(ax, 2).c_str(), String(ay, 2).c_str(), String(az, 2).c_str());
+    // Crear la cadena de solicitud HTTP con datos de GPS
+    String req_uri = "/update?x=" + String(ax, 2) +
+                     "&y=" + String(ay, 2) +
+                     "&z=" + String(az, 2) +
+                     "&lat=" + String(lat, 6) +
+                     "&lng=" + String(lng, 6);
+    client.print("GET " + req_uri + " HTTP/1.1\r\n" +
+                 "Host: " + server + "\r\n" +
+                 "Connection: close\r\n\r\n");
+    Serial.printf("Datos enviados - X: %s, Y: %s, Z: %s, Lat: %s, Lng: %s\n",
+                  String(ax, 2).c_str(),
+                  String(ay, 2).c_str(),
+                  String(az, 2).c_str(),
+                  String(lat, 6).c_str(),
+                  String(lng, 6).c_str());
   } else {
     Serial.println("Error al conectar con el servidor");
   }
   client.stop();
-  delay(sendingInternval);
+  delay(sendingInterval);
 }
 
-void Visualizacion_GPS(){ 
-   
-  if (gps.location.isValid() ==  1)
-  {  
+void Visualizacion_GPS() { 
+  if (gps.location.isValid()) {  
     Serial.print("Lat: ");
-    Serial.println(gps.location.lat(),6);
+    Serial.println(gps.location.lat(), 6);
     Serial.print("Lng: ");
-    Serial.println(gps.location.lng(),6);  
+    Serial.println(gps.location.lng(), 6);  
     Serial.print("Speed: ");
     Serial.println(gps.speed.kmph());    
     Serial.print("SAT:");
@@ -129,43 +130,32 @@ void Visualizacion_GPS(){
     Serial.print(gps.time.minute()); Serial.print(":");
     Serial.println(gps.time.second());
     Serial.println("---------------------------");
-  }
-  else
-  {
-    Serial.println("Sin señal gps");  
+  } else {
+    Serial.println("Sin señal GPS");  
   }  
 }
 
-//Esta parte del codigo se debe probar...
-void validarGPS(){
-    // This sketch displays information every time a new sentence is correctly encoded.
-  if (NMEA){
-    while (neogps.available())
-    {
-     datoCmd  = (char)neogps.read(); 
-     Serial.print (datoCmd );
+void validarGPS() {
+  // Esta parte del código se debe probar...
+  if (NMEA) {
+    while (neogps.available()) {
+      datoCmd = (char)neogps.read(); 
+      Serial.print(datoCmd);
     }
- } else{
-  boolean newData = false;
-  for (unsigned long start = millis(); millis() - start < 1000;)
-  {
-    while (neogps.available())
-    {
-      if (gps.encode(neogps.read()))
-      {
-        newData = true;         
+  } else {
+    boolean newData = false;
+    for (unsigned long start = millis(); millis() - start < 1000;) {
+      while (neogps.available()) {
+        if (gps.encode(neogps.read())) {
+          newData = true;         
+        }
       }
     }
-  }
 
-  if(newData == true){
-    newData = false;
-    Serial.println(gps.satellites.value());    
-    Visualizacion_GPS();
-  }
-  else{
-    
+    if (newData) {
+      newData = false;
+      Serial.println(gps.satellites.value());    
+      Visualizacion_GPS();
+    }
   }  
-  
- }  
 }
